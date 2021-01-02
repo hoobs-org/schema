3b7d0c4573618893f463975ab5846e19b1cf4494
mkellsy <mkellsy@gmail.com>
2021-01-01 17:12:31 -0700

    dynamically load field components

--- src/components/elements/schema.vue
+++ src/components/elements/schema.vue
@@ -19,24 +19,10 @@
 </template>
 
 <script>
-    import RootField from "@/components/fields/root.vue"
-    import FormField from "@/components/fields/form.vue"
-    import ListField from "@/components/fields/list.vue"
-    import OneOfField from "@/components/fields/oneof.vue"
-    import AnyOfField from "@/components/fields/anyof.vue"
-    import ButtonField from "@/components/fields/button.vue";
+    import { field } from "../../services/schema";
 
     export default {
         name: "schema",
-
-        components: {
-            "root-field": RootField,
-            "form-field": FormField,
-            "list-field": ListField,
-            "oneof-field": OneOfField,
-            "anyof-field": AnyOfField,
-            "button-field": ButtonField,
-        },
 
         props: {
             schema: Object,
@@ -47,23 +33,7 @@
 
         computed: {
             type() {
-                if (this.schema.widget === "button") return "button-field";
-                if (this.schema.widget === "textarea") return "textarea-field";
-
-                if (this.schema.oneOf !== undefined && Array.isArray(this.schema.oneOf)) return "oneof-field";
-                if (this.schema.enum !== undefined && Array.isArray(this.schema.enum)) return "select-field";
-
-                if (this.schema.type === "boolean") return "checkbox";
-                if (this.schema.type === "array" && this.schema.format === "root") return "root-field";
-                if (this.schema.type === "array") return this.schema.items.anyOf === undefined || !Array.isArray(this.schema.items.anyOf) ? "list-field" : "anyof-field";
-                if (this.schema.type === "object") return "form-field";
-                if (this.schema.type === "integer") return "integer-field";
-                if (this.schema.type === "number") return "number-field";
-
-                if (this.schema.format === "date") return "date-field";
-                if (this.schema.format === "password") return "password-field";
-
-                return "text-field";
+                return field(this.schema);
             },
 
             options() {
